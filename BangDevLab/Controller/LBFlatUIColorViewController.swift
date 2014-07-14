b0528493c545b35592b1b28cbc050247b4b17254
Bang Nguyen <bangnguyen@Bangs-MacBook-Pro.local>
2014-07-14 21:18:44 +0700

    Added FlatUIColor extension (with example)

--- BangDevLab/Controller/LBFlatUIColorViewController.swift
+++ BangDevLab/Controller/LBFlatUIColorViewController.swift
@@ -7,18 +7,74 @@
 //
 
 import UIKit
+import QuartzCore
 
-class LBFlatUIColorViewController: UIViewController {
+class LBFlatUIColorViewController: UIViewController, UIScrollViewDelegate {
 
-    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        // Custom initialization
+    @IBOutlet var _scrollView : UIScrollView
+    
+    init(coder aDecoder: NSCoder!)  {
+        super.init(coder: aDecoder)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        var colorArr = [
+            UIColor.flatTurquoiseColor(),   UIColor.flatGreenSeaColor(),
+            UIColor.flatEmeraldColor(),     UIColor.flatNephritisColor(),
+            UIColor.flatPeterRiverColor(),  UIColor.flatBelizeHoleColor(),
+            UIColor.flatAmethystColor(),    UIColor.flatWisteriaColor(),
+            UIColor.flatWetAsphaltColor(),  UIColor.flatMidNightColor(),
+            UIColor.flatSunFlowerColor(),   UIColor.flatOrangeColor(),
+            UIColor.flatCarrotColor(),      UIColor.flatPumpkinColor(),
+            UIColor.flatAlizarinColor(),    UIColor.flatPomegranateColor(),
+            UIColor.flatCloudColor(),       UIColor.flatSilverColor(),
+            UIColor.flatConcreteColor(),    UIColor.flatAsbestosColor()
+        ]
+        
+        var lastPos : CGFloat = self.view.bounds.width/2
+        
+        for var i = 0; i < colorArr.count; i++ {
+            
+            var view = UIView(frame: CGRectMake(0, 0, 280, 400))
+            view.center = CGPointMake(lastPos, self.view.bounds.height/2)
+            view.tag = i+1
+            view.backgroundColor = colorArr[i]
+            view.layer.cornerRadius = 8
+            
+            var scale = CABasicAnimation(keyPath: "transform.scale")
+            scale.fromValue = 1
+            scale.toValue = 0.5
+            scale.duration = 1
+            
+            var rotate = CABasicAnimation(keyPath: "transform.rotation")
+            rotate.fromValue = 0
+            rotate.toValue = -2
+            rotate.duration = 1
+            
+            var animation = CAAnimationGroup()
+            animation.animations = [ scale, rotate ]
+            animation.duration = 1
+            
+            view.layer.addAnimation(animation, forKey: "animate")
+            view.layer.speed = 0
+            
+            _scrollView.addSubview(view)
+            lastPos += self.view.bounds.width
+        }
+        
+        _scrollView.contentSize = CGSizeMake(self.view.bounds.width * CGFloat(colorArr.count), self.view.bounds.height)
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        _scrollView.backgroundColor = UIColor.clearColor()
+        _scrollView.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated);
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,15 +82,11 @@ class LBFlatUIColorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // #pragma mark - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        
+        // get index
+        var index = Int(scrollView.contentOffset.x / self.view.bounds.width)
+        var view = scrollView.viewWithTag(index+1)
+        view.layer.timeOffset = CFTimeInterval( abs( (scrollView.contentOffset.x - self.view.bounds.width * CGFloat(index)) / self.view.bounds.width ) )
     }
-    */
-
 }
--- BangDevLab/Lib/FlatUIColor.swift
+++ BangDevLab/Lib/FlatUIColor.swift
@@ -9,6 +9,71 @@
 import Foundation
 import UIKit
 
+/**
+*   Flat UI Color Extension for Swift
+*   Name and color base on http://flatuicolors.com
+*/
+
 extension UIColor {
     
+    class func flatTurquoiseColor() -> UIColor {
+        return UIColor(red: 26.0/255.0, green:  188.0/255.0, blue:  156.0/255.0, alpha: 1.0)
+    }
+    class func flatGreenSeaColor() -> UIColor {
+        return UIColor(red: 22.0/255.0, green:  160.0/255.0, blue:  133.0/255.0, alpha: 1.0)
+    }
+    class func flatEmeraldColor() -> UIColor {
+        return UIColor(red: 46.0/255.0, green: 204.0/255.0, blue: 113.0/255.0, alpha: 1.0)
+    }
+    class func flatNephritisColor() -> UIColor {
+        return UIColor(red: 39.0/255.0, green: 174.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+    }
+    class func flatPeterRiverColor() -> UIColor {
+        return UIColor(red: 52.0/255.0, green: 152.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+    }
+    class func flatBelizeHoleColor() -> UIColor {
+        return UIColor(red: 41.0/255.0, green: 128.0/255.0, blue: 185.0/255.0, alpha: 1.0)
+    }
+    class func flatAmethystColor() -> UIColor {
+        return UIColor(red: 155.0/255.0, green: 89.0/255.0, blue: 182.0/255.0, alpha: 1.0)
+    }
+    class func flatWisteriaColor() -> UIColor {
+        return UIColor(red: 142.0/255.0, green: 68.0/255.0, blue: 173.0/255.0, alpha: 1.0)
+    }
+    class func flatWetAsphaltColor() -> UIColor {
+        return UIColor(red: 52.0/255.0, green: 73.0/255.0, blue: 94.0/255.0, alpha: 1.0)
+    }
+    class func flatMidNightColor() -> UIColor {
+        return UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    }
+    class func flatSunFlowerColor() -> UIColor {
+        return UIColor(red: 241.0/255.0, green: 196.0/255.0, blue: 15.0/255.0, alpha: 1.0)
+    }
+    class func flatOrangeColor() -> UIColor {
+        return UIColor(red: 243.0/255.0, green: 156.0/255.0, blue: 18.0/255.0, alpha: 1.0)
+    }
+    class func flatCarrotColor() -> UIColor {
+        return UIColor(red: 230.0/255.0, green: 126.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+    }
+    class func flatPumpkinColor() -> UIColor {
+        return UIColor(red: 211.0/255.0, green: 84.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    }
+    class func flatAlizarinColor() -> UIColor {
+        return UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+    }
+    class func flatPomegranateColor() -> UIColor {
+        return UIColor(red: 192.0/255.0, green: 57.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+    }
+    class func flatCloudColor() -> UIColor {
+        return UIColor(red: 236.0/255.0, green: 240.0/255.0, blue: 241.0/255.0, alpha: 1.0)
+    }
+    class func flatSilverColor() -> UIColor {
+        return UIColor(red: 189.0/255.0, green: 195.0/255.0, blue: 199.0/255.0, alpha: 1.0)
+    }
+    class func flatConcreteColor() -> UIColor {
+        return UIColor(red: 149.0/255.0, green: 165.0/255.0, blue: 166.0/255.0, alpha: 1.0)
+    }
+    class func flatAsbestosColor() -> UIColor {
+        return UIColor(red: 127.0/255.0, green: 140.0/255.0, blue: 141.0/255.0, alpha: 1.0)
+    }
 }
