36aa7140a591ac10cc9a86d641b038de4da49961
Bang Nguyen <bangnguyen@Bangs-MacBook-Pro.local>
2014-07-14 18:03:50 +0700

    first commit

---
+++ BangDevLab/Lib/FlatUIColor.swift
@@ -0,0 +1,14 @@
+//
+//  FlatUIColor.swift
+//  BangDevLab
+//
+//  Created by Bang Nguyen on 14/07/2014.
+//  Copyright (c) Năm 2014 Bang Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+}
