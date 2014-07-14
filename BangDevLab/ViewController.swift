749ae582de0f111da807ea2f250041410463cabc
Bang Nguyen <bangnguyen@Bangs-MacBook-Pro.local>
2014-07-14 17:49:17 +0700

    Initial Commit

---
+++ BangDevLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BangDevLab
+//
+//  Created by Bang Nguyen on 14/07/2014.
+//  Copyright (c) Năm 2014 Bang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
